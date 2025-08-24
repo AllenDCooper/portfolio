d530aaa9c8a3278d01922931837b41738f5a1cd5
Allen Cooper <allendcooper@gmail.com>
2025-08-24 10:02:34 -0400

    build: set base to '/' for custom DNS

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,8 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig(() => {
-  const base = process.env.NODE_ENV === "development" ? "/" : "/portfolio"
+  // const base = process.env.NODE_ENV === "development" ? "/" : "/portfolio"
+  const base = "/"
 
   return ({
     base: base,
